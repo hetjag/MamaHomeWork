bb322afe84fdb78c61945d54a81bbf8fd0398383
hetjag <Hetaldhameliya13@gmail.com>
2019-12-20 16:09:35 +0000

    created empty project

---
+++ src/MamaGit.java
@@ -0,0 +1,33 @@
+public class MamaGit {
+
+    int j = 30;  // instance variable
+    static int b = 40;  // declared inside the class and outside the methods - with static keyword--> class/static variable
+    static int d = 20;
+
+    //No return type - No parameters INSTANCE method
+    public void m1() {
+        System.out.println(j);
+        System.out.println(b);
+    }
+
+
+
+
+
+    // No return type - with parameters STATIC \method
+    public static void subtraction(int l, int m) {
+        System.out.print("subtraction of " + l + " and " + m + " = ");
+        System.out.println(l - m);
+        System.out.println("subtraction of " + b + " and " + d + " = " + (b - d));
+    }
+
+    //main method
+    public static void main(String[] args) {
+        String company = "TFL"; //local variable - declared inside the method
+        System.out.println(company);
+        subtraction(20, 10);
+        subtraction(100, 300);
+        
+        subtraction(100, 60);
+    }
+}
